da92d21345d20f7f3a15a329dec930b6df5a44bc
unknown <ikvinarizqo18@gmail.com>
2024-06-24 14:27:44 +0700

    membuat fungsi update

--- app/Http/Controllers/KotaController.php
+++ app/Http/Controllers/KotaController.php
@@ -5,14 +5,12 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Kota;
 
-
 class KotaController extends Controller
 {
     public function index () {
         $listKota = Kota::get();
         return view('kota.index',compact('$listKota'));
     }
-
     public function showFormAdd(){
         return view('kota.add');
     }
@@ -30,7 +28,25 @@ class KotaController extends Controller
         $kota->save();
 
         return redirect()->route('list-kota')
-        ->with('success', 'mesin Berhasil Di tambahkan');
+        ->with('success', 'Kota Berhasil Di tambahkan');
+    }
+    public function showFormEdit ($id) {
+        $listKota = Kota::where('id_kota',$id)->get();
+        return view('kota.edit',compact('$listKota'));
+    }
+
+    public function proccesEdit(Request $request){
+        $kota = Kota::update([
+            'nama_kota' => $request->nama_kota,
+            'nama_pemimpin' => $request->nama_pemimpin,
+            'tanggal_berdiri' => $request->tanggal_berdiri,
+            'jumlah_penduduk' => $request->jumlah_penduduk,
+            'luas_wilayah' => $request->luas_wilayah,
+            'status' => $request->status,
+            'keunggulan' => $request->keunggulan,
+        ]);
+
+        return redirect()->route('list-kota')
+        ->with('success', 'Kota Berhasil Di tambahkan');
     }
 }
-
